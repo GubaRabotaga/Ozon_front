23649cf380cde89363f5b9023af0c45b49bb8640
Ivan <ivangybskiy03@gmail.com>
2022-12-07 17:04:50 +0300

    OzonTest

---
+++ src/App.js
@@ -0,0 +1,55 @@
+
+import React,{useState}  from 'react';
+import DataFetching from './components/DataFetching';
+
+function App() {
+ const[delivery,setDelivery]=useState();
+  return (
+
+  <div className='top_select'> 
+
+  
+     <span className='top_category'>Обувь<img width={30} height={30} src="img/arrow-right.svg"></img> Мужская обувь</span>
+          <div className='main_select'><h1>Кеды и слипоны мужские</h1> <span>20 826 товаров</span></div>
+          
+          <div className='categories'>
+             <h4>Категории</h4>
+             <form>Популярныеㅤㅤㅤㅤㅤ ▾</form>
+             <span className='spaned'>Обувь</span>
+             <span className='spaned'>Мужчинам</span>
+             
+             </div>
+
+             <div className='products'>
+             <span  >Кеды кроссовки и слипоны  </span>
+             <span >слипоны</span>
+             <span >Кеды</span>
+             <span >кроссовки</span>
+             <h4>Сроки доставки</h4>
+             
+             <input type="radio" name="delivery" value="doesnmatter" onChange={e=>setDelivery(e.target.value)}/><form>Неважно</form>
+             <input type="radio" name="delivery" value="tomorrow" onChange={e=>setDelivery(e.target.value)}/> <img width={30} height={30} src="img/rocket.svg"></img><form>ㅤЗавтра</form>
+             <input type="radio" name="delivery" value="2day" onChange={e=>setDelivery(e.target.value)}/> <form>До 2 дней</form>
+             <input type="radio" name="delivery" value="5day" onChange={e=>setDelivery(e.target.value)}/><form> До 5 дней</form>
+             <h4>Российский размер(обуви)</h4>
+             <div className='squareradio'>
+             <img width={30} height={30} src="img/choose.svg"></img> <label for="radioA"><input type="radio" name="radio1" value="A"/> 40</label><br/>
+             <img width={30} height={30} src="img/choose.svg"></img><label for="radioB"><input type="radio" name="radio1" value="B"/> 41</label><br/>
+             <img width={30} height={30} src="img/choose.svg"></img><label for="radioC"><input type="radio" name="radio1" value="C"/> 42</label><br/>
+             <img width={30} height={30} src="img/choose.svg"></img> <label for="radioD"><input type="radio" name="radio1" value="D"/> 43</label><br/>
+             <img width={30} height={30} src="img/choose.svg"></img> <label for="radioE"><input type="radio" name="radio1" value="E"/> 44</label>
+              </div>
+              <h5>Посмотреть все</h5>
+             </div>
+             <span className='App'>
+             
+               <DataFetching /><br/>
+                  
+             </span> 
+   </div>
+   
+    );
+  
+}
+
+export default App;
---
+++ src/components/DataFetching.js
@@ -0,0 +1,62 @@
+import React,{useState,useEffect} from "react";
+import axios from 'axios';
+import './title.scss';
+
+<style>
+</style>
+
+function DataFetching(){
+const [posts,setPosts] = useState([])
+useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products')
+    .then(res=> {
+        
+        setPosts(res.data)
+    })
+    .catch(err=> {
+        console.log(err)
+    })
+},[])
+
+    
+    const newLocal = "220";
+    return(
+    <div>
+  <ul>
+  
+    {
+        
+        posts.map(post=> <li key={post.id}>
+        <img width={newLocal} height="250" src={post.image}/>
+        <span className="title">
+        <span className="price">
+        {post.price}$
+        </span>
+        <span className="oxontext"> с OZON картой</span>
+        <br/>
+        <span className="realprice"> {post.price+(post.price/5)}</span>
+        <span className="unrealprice"> {post.price+(post.price*3)}</span>
+      
+        <br/>
+        
+        {post.title}
+        <br/>
+        
+        <img className="star" width={16} height={16} src="img/star.svg"/><span className="rate"> {post.rating.rate}</span>
+        <img className="star" width={16} height={16} src="img/comment.svg"/><span className="rate"> {post.rating.count}</span>
+        </span>
+        
+        
+        </li>)
+        
+    }
+    
+  </ul>
+    </div>
+
+
+    )
+    
+}
+
+export default DataFetching
